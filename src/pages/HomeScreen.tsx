b4317be4a33bc75f106ea877460ee4eec4889d4c
CreatyJohnKwon <vmfodzl1125@naver.com>
2025-02-21 04:48:05 +0900

    📦 tailwindcss 패키지 수정 완료

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -8,6 +8,9 @@ const HomeScreen = () => {
         <h2>말씀 위에 세운 교회</h2>
         <p>Ephesians 2 : 20</p>
         <p>찾아오시는 길</p>
+        <button className="dark:md:hover:bg-gray-300">
+          Save changes
+        </button>
        </main>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2bca43",
+        },
+      },
+    },
   },
   plugins: [],
 }
