bfbbac7b0bf894e09a7fcda61df57861a1422525
CreatyJohnKwon <vmfodzl1125@naver.com>
2025-02-21 15:12:04 +0900

    ✨ 다크모드 기능 추가 및 로고 assets 추가; 네비게이션 바 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import DarkMode from './features/DarkMode';
 import HomeScreen from './pages/HomeScreen';
+import Navbar from './pages/Navbar';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <HomeScreen />
+      <DarkMode />
     </>
   )
 }
 
-export default App
+export default App;
