895982384f7b4eb116c39e733e2faa3ddb077bb3
CreatyJohnKwon <vmfodzl1125@naver.com>
2025-02-21 18:51:25 +0900

    ✨ Add custom screen sizes and update TypeScript module setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      screens: {
+        "c_md": "850px",
+        "c_sm":"360px"
+      },
       colors: {
         primary: {
           DEFAULT: "#2bca43",
