766cd0b68a17e9ae314dc4d10b8e9ebe4e7df36d
woc-dev-team <wordofchurch22@gmail.com>
2025-02-25 17:52:27 +0900

    📦 그라데이션 정리

--- src/pages/HomeChildren/AboutPastor/PastorTexts.tsx
+++ src/pages/HomeChildren/AboutPastor/PastorTexts.tsx
@@ -14,9 +14,9 @@ const PastorTexts = ({profileImg}: PastorTextsProps) => {
             </div>
             <p className="text-lg c_sm:text-xl c_md:text-3xl c_md:mt-12">The Word Church 담임 목사</p>
             <p className="text-lg c_sm:text-xl c_md:text-3xl mt-5">_Of Church 사역팀 대표 목사</p>
-            <p className="text-lg c_sm:text-xl c_md:text-3xl mt-20 font-thin">총신대학교 신학대학원</p>
-            <p className="text-lg c_sm:text-xl c_md:text-3xl mt-10 font-thin">총신대학교 일반대학원 석사 (Th.M 조직신학)</p>
-            <p className="text-lg c_sm:text-xl c_md:text-3xl mt-10 font-thin">총신대학교 일반대학원 박사과정 (Ph.D 조직신학)</p>
+            <p className="text-lg c_sm:text-xl c_md:text-3xl mt-14 c_md:mt-20 font-thin">총신대학교 신학대학원</p>
+            <p className="text-lg c_sm:text-xl c_md:text-3xl mt-8 font-thin">총신대학교 일반대학원 석사 (Th.M 조직신학)</p>
+            <p className="text-lg c_sm:text-xl c_md:text-3xl mt-8 font-thin">총신대학교 일반대학원 박사과정 (Ph.D 조직신학)</p>
         </div>
     )
 }
--- src/pages/HomeChildren/MainElements/ChurchImages.tsx
+++ src/pages/HomeChildren/MainElements/ChurchImages.tsx
@@ -8,7 +8,8 @@ const ChurchImages = () => {
                 alt="교회 이미지"
                 className="w-full h-screen object-cover"
             />
-            <div className="absolute inset-y-0 left-0 w-3/5 bg-gradient-to-r from-white dark:from-black to-transparent" />
+            <div className="absolute top-0 left-0 w-full h-2/6 bg-gradient-to-b from-white dark:from-black to-transparent" />
+            <div className="absolute inset-y-0 left-0 w-2/6 bg-gradient-to-r from-white dark:from-black to-transparent" />
             <div className="absolute bottom-0 left-0 w-full h-3/5 bg-gradient-to-b from-transparent to-white dark:to-black" />
         </>
     )
