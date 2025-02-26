15f63f049ae4e85de2947f8f6e4d7ae7cec22857
woc-dev-team <wordofchurch22@gmail.com>
2025-02-27 01:49:28 +0900

    ✨ 리액트 플러그인 버전 업데이트 vite.config & package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: "/twcommunity/",
