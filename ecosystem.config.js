6ca3455826aee5766fb3d95035774a1596d66f8b
woc-dev-team <wordofchurch22@gmail.com>
2025-03-05 17:22:52 +0900

    ✨ ecosystem.config.js에서 애플리케이션 진입점 경로를 상대 경로로 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ export default {
     apps: [
       {
         name: "twc-server",   // 애플리케이션 이름
-        script: "src/index.ts",  // 애플리케이션의 진입점 (src/index.ts로 설정)
+        script: "./src/index.ts",  // 애플리케이션의 진입점 (src/index.ts로 설정)
         cwd: "/home/ubuntu/twcommunity",  // 프로젝트 디렉토리 경로 지정
         interpreter: "/home/ubuntu/.nvm/versions/node/v22.14.0/bin/node", // Node.js 경로 지정 (필요 시)
         exec_mode: "fork",   // 단일 프로세스로 실행
