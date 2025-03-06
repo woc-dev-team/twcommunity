1a4dc70b90e61208b37ea4b702d04e405ed5b0f6
woc-dev-team <wordofchurch22@gmail.com>
2025-03-07 04:00:57 +0900

    ✨ useBlog.ts에서 fetch 요청의 CORS 모드를 'no-cors'로 변경

--- src/features/NaverBlogs/useBlog.ts
+++ src/features/NaverBlogs/useBlog.ts
@@ -12,7 +12,7 @@ const useBlog = () => {
             const response = await fetch("https://twcommunity-server.store/search/blog?query=더워드", {
                 method: "GET",
                 credentials: "include",
-                mode: 'cors',
+                mode: 'no-cors',
                 headers: {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': 'http://172.30.1.12:5173',
