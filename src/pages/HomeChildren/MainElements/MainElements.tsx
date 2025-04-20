df0380a7037a4d55c7a34b137b43cf15bdddb548
CreatyJohnKwon <vmfodzl1125@naver.com>
2025-04-20 14:56:19 +0900

    ✨ refactor: 이미지 업데이트
    * 이미지를 업데이트 수정
    
    resolves: #3

--- src/pages/HomeChildren/MainElements/MainElements.tsx
+++ src/pages/HomeChildren/MainElements/MainElements.tsx
@@ -17,7 +17,7 @@ const MainElements = () => {
                 <NaverMaps className="w-full h-full" />
             </Modal>
             
-            <div className="h-screen grid grid-rows-6 c_image:grid-cols-10 gap-4 text-start transition-all duration-500 ease-in-out">
+            <div className="h-full grid c_md:grid-rows-6 c_image:grid-cols-10 gap-4 text-start transition-all duration-500 ease-in-out">
                 <div className="h-full p-5 c_md:ps-20 c_md:pt-20 row-span-2 c_image:col-span-4 z-10">
                     <MainTexts />
                 </div>
@@ -26,7 +26,7 @@ const MainElements = () => {
                     <CustomImage className="w-full h-full object-cover transition-all duration-500 ease-in-out" src={ChurchImage} alt={"교회 메인 이미지"} gradation="tbl"/>
                 </div>
                 {/* 모바일 버전 */}
-                <div className="relative row-span-4 h-screen c_image:col-span-6 c_image:hidden block">
+                <div className="relative row-span-4 h-full c_image:col-span-6 c_image:hidden block">
                     <CustomImage className="w-full h-full object-cover transition-all duration-500 ease-in-out" src={ChurchImage} alt={"교회 메인 이미지"} gradation="tb"/>
                 </div>
             </div>
