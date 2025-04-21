d8d4366e77ecb1d42f4d29d076b87e0157f265bd
CreatyJohnKwon <vmfodzl1125@naver.com>
2025-04-21 18:46:26 +0900

    :bug: bug: 사파리 UI 버그
    * 사파리에서 발견된 QR 코드 설정란, 콘텐츠 정렬 오류
    * 해결방안, 중앙정렬을 명시해줌
    
    resolves: #4

--- src/pages/HomeChildren/AboutChurch/Actives/Counsel.tsx
+++ src/pages/HomeChildren/AboutChurch/Actives/Counsel.tsx
@@ -15,7 +15,7 @@ const Counsel = ({title}: {title: string}) => {
                     <button className="w-2/5" onClick={() => startPopUp(languagePacks[languageIndex].aboutChurch.counsel.nurture.title, "https://docs.google.com/forms/d/e/1FAIpQLSclOc3ENeRSNp3gzyt3Xruvfp3ntuvCjuJIoXKeXr-lSIpAkw/viewform")}>
                         <img src={QRNurture} className="place-self-center rounded-lg hover:scale-105 transition-all duration-500 ease-in-out" />
                     </button>
-                    <div className="h-full w-full">
+                    <div className="h-full w-full flex flex-col justify-center">
                         <p className="h-auto w-full text-black dark:text-white text-lg c_md:text-2xl font-sans pt-2 c_md:pt-5">{languagePacks[languageIndex].aboutChurch.counsel.nurture.title}</p>
                         <p className="h-auto w-4/5 place-self-center text-black dark:text-white text-base c_md:text-lg font-thin pt-2">{languagePacks[languageIndex].aboutChurch.counsel.nurture.desciption}</p>
                     </div>
@@ -24,7 +24,7 @@ const Counsel = ({title}: {title: string}) => {
                     <button className="w-2/5" onClick={() => startPopUp(languagePacks[languageIndex].aboutChurch.counsel.counseling.title, "https://docs.google.com/forms/d/e/1FAIpQLSfHjzaTOq0CYpptmyrdpmGNFu6oVOV9Qcf-v1p_w6oE7X1z8w/viewform")}>
                         <img src={QRCounseling} className="place-self-center rounded-lg hover:scale-105 transition-all duration-500 ease-in-out" />
                     </button>
-                    <div className="h-full w-full">
+                    <div className="h-full w-full flex flex-col justify-center">
                         <p className="h-auto w-full text-black dark:text-white text-lg c_md:text-2xl font-sans pt-2 c_md:pt-5">{languagePacks[languageIndex].aboutChurch.counsel.counseling.title}</p>
                         <p className="h-auto w-4/5 place-self-center text-black dark:text-white text-base c_md:text-lg font-thin pt-2">{languagePacks[languageIndex].aboutChurch.counsel.counseling.desciption}</p>
                     </div>
